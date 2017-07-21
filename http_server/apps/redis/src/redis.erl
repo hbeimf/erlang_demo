1b94c8a47a6f87ac4749289131cd2f67ba9edbab
maofeng <maofeng@jrq.com>
2017-07-21 10:59:41 +0800

    redis demo

--- http_server/apps/redis/src/redis.erl
+++ http_server/apps/redis/src/redis.erl
@@ -2,7 +2,41 @@
 -compile(export_all).
 
 get() ->
-    % redis:q(pool1, ["SET", "foo", "bar"]).
     redis_call:q(pool1, ["GET", "foo"]).
 
+set() ->
+    redis_call:q(pool1, ["SET", "foo", "bar"]).
 
+
+
+lpush() ->
+    lpush("list100", "val100").
+lpush(ListName, Val) ->
+    redis_call:q(pool1, ["LPUSH", ListName, Val]).
+
+rpop() ->
+    rpop("list100").
+rpop(ListName) ->
+    redis_call:q(pool1, ["RPOP", ListName]).
+
+% (http_server@127.0.0.1)1> redis:lpush().
+% {ok,<<"2">>}
+% (http_server@127.0.0.1)2> redis:rpop().
+% {ok,<<"value222">>}
+% (http_server@127.0.0.1)3> redis:rpop().
+% {ok,<<"val100">>}
+% (http_server@127.0.0.1)4> redis:rpop().
+% {ok,undefined}
+% (http_server@127.0.0.1)5> redis:rpop().
+% {ok,undefined}
+% (http_server@127.0.0.1)6> redis:lpush().
+% {ok,<<"1">>}
+% (http_server@127.0.0.1)7> redis:lpush().
+% {ok,<<"2">>}
+% (http_server@127.0.0.1)8> redis:rpop().
+% {ok,<<"val100">>}
+% (http_server@127.0.0.1)9> redis:rpop().
+% {ok,<<"val100">>}
+% (http_server@127.0.0.1)10> redis:rpop().
+% {ok,undefined}
+% (http_server@127.0.0.1)11>

31f0e75a4ceb866c061535afd9d8f2a2ac99222b
root <root@localhost.localdomain>
2017-08-25 19:38:22 -0700

    conn

--- connection/apps/connection/src/connection_protocol.erl
+++ connection/apps/connection/src/connection_protocol.erl
@@ -15,7 +15,7 @@
 
 -define(TIMEOUT, 5000).
 
--record(state, {socket, transport}).
+-record(state, {socket, transport, data}).
 
 %% API.
 
@@ -32,15 +32,30 @@ init({Ref, Socket, Transport, _Opts = []}) ->
 	ok = ranch:accept_ack(Ref),
 	ok = Transport:setopts(Socket, [{active, once}]),
 	gen_server:enter_loop(?MODULE, [],
-		#state{socket=Socket, transport=Transport},
+		#state{socket=Socket, transport=Transport, data=<<>>},
 		?TIMEOUT).
 
-handle_info({tcp, Socket, Data}, State=#state{
-		socket=Socket, transport=Transport})
-		when byte_size(Data) > 1 ->
+handle_info({tcp, Socket, CurrentPackage}, State=#state{
+		socket=Socket, transport=Transport, data=LastPackage})
+		% when byte_size(Data) > 1 ->
 	Transport:setopts(Socket, [{active, once}]),
-	Transport:send(Socket, reverse_binary(Data)),
-	{noreply, State, ?TIMEOUT};
+	PackageBin = <<LastPackage/binary, CurrentPackage/binary>>,
+
+	case parse_package(PackageBin) of
+		{ok, waitmore} -> 
+			{noreply, State#state{data = PackageBin}};
+		{ok, RightPackage, NextPageckage} -> 
+			% parse logic and reply here ========================
+			% Transport:send(Socket, reverse_binary(RightPackage)),
+			io:format("package here ========== ~p~n ", [RightPackage]),
+
+			{noreply, State#state{data = NextPageckage}};			
+		_ -> 
+			{stop, stop_noreason,State}
+	end;
+
+	% Transport:send(Socket, reverse_binary(Data)),
+	% {noreply, State, ?TIMEOUT};
 handle_info({tcp_closed, _Socket}, State) ->
 	{stop, normal, State};
 handle_info({tcp_error, _, Reason}, State) ->
@@ -64,7 +79,30 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% Internal.
 
-reverse_binary(B) when is_binary(B) ->
-	[list_to_binary(lists:reverse(binary_to_list(
-		binary:part(B, {0, byte_size(B)-2})
-	))), "\r\n"].
+% reverse_binary(B) when is_binary(B) ->
+% 	[list_to_binary(lists:reverse(binary_to_list(
+% 		binary:part(B, {0, byte_size(B)-2})
+% 	))), "\r\n"].
+
+
+parse_package(PackageBin) when erlang:byte_size(PackageBin) >= 2 ->
+	case parse_head(PackageBin) of
+		{ok, PackageLen} ->	
+			parse_body(PackageLen, PackageBin);
+		_ ->	
+			{ok, waitmore}
+	end.
+parse_package(_) ->
+	{ok, waitmore}. 
+
+parse_head(<<PackageLen:16 ,_/binary>> )
+	{ok, PackageLen};
+parse_head(_) ->
+	error.
+
+parse_body(PackageLen, PackageBin) ->
+	case PackageBin of 
+		<<Package:PackageLen/binary,LefBin/binary>> ->
+			{ok, RightPackage , NextPageckage};
+		_ -> {ok, waitmore}
+	end.
